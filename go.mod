df1482dbda6e64f030e4868b62372ed91c44981f
ansark88 <ansark88@gmail.com>
2021-03-13 21:55:54 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module rim_mod_translate_downloader_go_edition
+
+go 1.16
+
+require github.com/spf13/cobra v1.1.3 // indirect
---
+++ internal/userpath.go
@@ -0,0 +1,9 @@
+package UserPath
+
+type UserPath struct {
+	workshop_dir string
+}
+
+func (u UserPath) decide_workshop_dir() string {
+	return ""
+}
